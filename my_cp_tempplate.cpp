d70547c3480eff3bcd4f6f092a0cfa43221a67a5
mitparmarcse <mitparmarcse@gmail.com>
2025-09-20 06:37:25 +0530

    add CP template for c++ language

---
+++ my_cp_tempplate.cpp
@@ -0,0 +1,22 @@
+// Author: mit_1618
+#include<bits/stdc++.h>
+#define endl '\n'
+#define for(lb,ub) for(size_t i=lb;i<ub;i++)
+#define rfor(ub,lb) for(size_t i=ub;i>=lb;i--)
+using ll=long long;
+using namespace std;
+
+void solve(){
+
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+	ll test;	cin>>test;
+	while(test--){
+		solve();
+	}
+
+	return 0;
+}
